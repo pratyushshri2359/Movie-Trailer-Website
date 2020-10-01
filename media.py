1ac7dcc23e38e43812eb2796ac899b32a8e0d569
pratyushshri2359 <47253449+pratyushshri2359@users.noreply.github.com>
2020-10-01 19:05:55 +0530

    Updated Poster of NYSM

--- media.py
+++ media.py
@@ -6,7 +6,7 @@ fandf7 = project.movie("Furious 7","https://2.bp.blogspot.com/-KCErRWJaS40/VYKwV
 fandf8 = project.movie("Fast 8", "http://www.fotolip.com/wp-content/uploads/2016/05/Fast-And-Furious-8-1.jpg", "https://www.youtube.com/watch?v=uisBaTkQAEs","2017","9.0")
 inters = project.movie("Interstellar","http://www.movienewz.com/img/gallery/interstellar/posters/interstellar_movie_poster_4.jpg","https://www.youtube.com/watch?v=0vxOhd4qlnA","2014","8.6")
 darkknight = project.movie("The Dark Knight","https://akalol.files.wordpress.com/2008/08/new-joker-poster-for-the-dark-knight.jpg","https://www.youtube.com/watch?v=_PZpmTj1Q8Q","2008","9.0")
-nysm = project.movie("Now You See Me","https://image.tmdb.org/t/p/original/hGsi9bPp4PEQANCUxswQDLymJag.jpg","https://www.youtube.com/watch?v=4OtM9j2lcUA","2013","7.3")
+nysm = project.movie("Now You See Me","https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/Now-You-See-Me-Live.jpg","https://www.youtube.com/watch?v=4OtM9j2lcUA","2013","7.3")
 movies = [three_idiots, fandf7, fandf8, inters, darkknight, nysm]
 movies=sorted(movies, key=lambda movie:movie.name)
 fresh_tomatoes.open_movies_page(movies)
